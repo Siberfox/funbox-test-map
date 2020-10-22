4471d43b3add46cf4b0e27151da0c6880a48c06c
Andrew I <greyfoxjob@gmail.com>
2020-10-22 13:16:20 +0600

    initialize repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import GoogleMapReact from "google-map-react";
+
+import { Map, MapNavPanel, MapWrapper } from "./App.styles.jsx";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function App() {
+  const [mapConfig, setMapConfig] = useState({
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  });
+
+  const mapKey = "AIzaSyAcLjLdaRerLI85DDCDyIQHr1qUflc_NwI";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map>
+        <MapNavPanel />
+        <MapWrapper>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: mapKey }}
+            defaultCenter={mapConfig.center}
+            defaultZoom={mapConfig.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </MapWrapper>
+      </Map>
     </div>
   );
 }
---
+++ src/App.styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MapWrapper = styled.div`
+  width: 70vw;
+  height: 100vh;
+`;
+
+export const Map = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const MapNavPanel = styled.div`
+  height: 100vh;
+  width: 20vw;
+`;
