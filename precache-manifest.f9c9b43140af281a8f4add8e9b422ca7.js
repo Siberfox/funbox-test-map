1fb785ab27b4a542d3418784b389f4f1e20da724
Andrew I <greyfoxjob@gmail.com>
2020-10-23 11:26:53 +0600

    Updates

---
+++ precache-manifest.f9c9b43140af281a8f4add8e9b422ca7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfb64b66e394fde6501c470b86acb41",
+    "url": "/funbox-test-map/index.html"
+  },
+  {
+    "revision": "e1eb413e0c5a2ed8e783",
+    "url": "/funbox-test-map/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "676a62526c689a41af80",
+    "url": "/funbox-test-map/static/css/main.8e18de48.chunk.css"
+  },
+  {
+    "revision": "e1eb413e0c5a2ed8e783",
+    "url": "/funbox-test-map/static/js/2.b55fbf75.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/funbox-test-map/static/js/2.b55fbf75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676a62526c689a41af80",
+    "url": "/funbox-test-map/static/js/main.732c13ce.chunk.js"
+  },
+  {
+    "revision": "904837fb79242f5f4d1e",
+    "url": "/funbox-test-map/static/js/runtime-main.298f79f9.js"
+  },
+  {
+    "revision": "7279aed03f368907acbb393e1cc11ffd",
+    "url": "/funbox-test-map/static/media/Pattern.7279aed0.png"
+  }
+]);
